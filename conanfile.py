dcc6fc62c15c29382ba3aafd93ba814c6fa7fab7
Zalez95 <12982523+Zalez95@users.noreply.github.com>
2025-02-05 18:49:46 +0100

    Updated build scripts to conan2

--- conanfile.py
+++ conanfile.py
@@ -1,8 +1,10 @@
-from conans import ConanFile, CMake
+from conan import ConanFile
+from conan.tools.files import copy
+from conan.tools.cmake import cmake_layout, CMake, CMakeToolchain, CMakeDeps
 
 class SombraPhysicsConan(ConanFile):
-	name = "SombraPhysics"
-	version = "v1.0"
+	name = "sombra-physics"
+	version = "v1.0.0"
 	license = "MIT"
 	author = "Zalez95"
 	settings = "os", "compiler", "build_type", "arch"
@@ -11,27 +13,39 @@ class SombraPhysicsConan(ConanFile):
 		"fPIC" : [True, False],
 		"test" : [True, False]
 	}
-	default_options = {"shared": False, "fPIC": True, "test": False}
-	generators = "cmake"
+	default_options = { "shared" : False, "fPIC" : True, "test" : False }
 
 	def requirements(self):
-		self.requires("stdext/v1.0")
-		self.requires("glm/0.9.9.8")
+		self.requires("glm/0.9.9.8", transitive_headers=True)
+		self.requires("stdext/v1.0.0", transitive_headers=True)
 		if self.options.test:
-			self.requires("gtest/1.10.0")
+			self.requires("gtest/1.15.0")
 
 	def config_options(self):
 		if self.settings.os == "Windows":
 			del self.options.fPIC
 		self.options["gtest"].shared = True
 
+	def layout(self):
+		cmake_layout(self)
+
 	def export_sources(self):
-		self.copy("*")
+		copy(self, "LICENSE", self.recipe_folder, self.export_sources_folder)
+		copy(self, "*.txt", self.recipe_folder, self.export_sources_folder)
+		copy(self, "*.cpp", self.recipe_folder, self.export_sources_folder)
+		copy(self, "*.h", self.recipe_folder, self.export_sources_folder)
+		copy(self, "*.hpp", self.recipe_folder, self.export_sources_folder)
+
+	def generate(self):
+		tc = CMakeToolchain(self)
+		tc.variables["SOMBRA_PHYSICS_BUILD_TEST"] = self.options.test
+		tc.variables["SOMBRA_PHYSICS_BUILD_DOC"] = False
+		tc.generate()
+		deps = CMakeDeps(self)
+		deps.generate()
 
 	def build(self):
 		cmake = CMake(self)
-		cmake.definitions["SOMBRA_PHYSICS_BUILD_TESTS"] = self.options.test
-		cmake.definitions["SOMBRA_PHYSICS_BUILD_DOC"] = False
 		cmake.configure()
 		cmake.build()
 
@@ -40,6 +54,4 @@ class SombraPhysicsConan(ConanFile):
 		cmake.install()
 
 	def package_info(self):
-		self.cpp_info.includedirs = ["include"]
-		self.cpp_info.libdirs = ["lib"]
-		self.cpp_info.libs = [self.name]
+		self.cpp_info.libs = ["SombraPhysics"]
